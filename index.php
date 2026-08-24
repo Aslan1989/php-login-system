bad019af34af992ad8bb01d2a46bc8ef22108db8
AslanIsaev <bonscott.isaev@gmail.com>
2026-08-24 22:16:19 +0200

    created connection with mysql using XAAMP

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP Login System</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<h1>PHP Login System</h1>
+
+    <p>Welcome to my PHP authentication project.</p>
+</body>
+</html>
