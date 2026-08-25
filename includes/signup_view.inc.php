06c242f9093e76b217156848cc960072f5cdb135
AslanIsaev <bonscott.isaev@gmail.com>
2026-08-25 21:48:20 +0200

    create sigup system

--- includes/signup_view.inc.php
+++ includes/signup_view.inc.php
@@ -7,11 +7,14 @@ function check_signup_errors() {
 		$errors = $_SESSION["errors_signup"];
 
 		echo "<br>";
+
 		foreach ($errors as $error){
 			echo '<p class="form-error">' . $error . '</p>';
 		}
+
 		unset($_SESSION["errors_signup"]);
-	} else if (isset($_GET["signup"]) && isset($_GET["signup"]) === "success") {
+
+	} else if (isset($_GET["signup"]) && $_GET["signup"] === "success") {
 		echo '<br>';
 		echo '<p class="form-success">Signup success!</p>';
 	}
